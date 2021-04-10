a0bd88615301a8fc9baf0e9c68bc2f6b2b1681a6
Anthony Sharp <asharp343@gmail.com>
2021-04-10 14:44:08 -0600

    partial pagination

---
+++ INTEXII_App/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace INTEXII_App.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public PagingInfo()
+        {
+        }
+
+        public int TotalNumItems { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages => (int)(Math.Ceiling((decimal)TotalNumItems / ItemsPerPage));
+    }
+}
